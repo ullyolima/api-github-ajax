b2e9b66d674bd72d99f800d1fce02103352e57c4
ullyolima <ullyolima@ullyolima.box>
2021-02-21 22:44:53 -0300

    Add promise

--- script.js
+++ script.js
@@ -6,28 +6,39 @@ const pLocation = document.querySelector(".js-location")
 const pUrl = document.querySelector(".js-url")
 
 function recoverData() {
-    const xhr = new XMLHttpRequest()
 
-    xhr.open("GET", "https://api.github.com/users/ullyolima")
-    xhr.send(null)
-    xhr.onreadystatechange = () => {
-        if(xhr.readyState === 4) {
-            const res = JSON.parse(xhr.responseText)
-            console.log(res)
-
-            const name = res.name
-            const avatar = res.avatar_url
-            const url = res.html_url 
-            const location = res.location 
-            const login = res.login 
-            const bio = res.bio
-
-            htmlImg.setAttribute("src", avatar)
-            h2Name.innerHTML = name
-            pLogin.innerHTML = login
-            pBio.innerHTML = bio
-            pLocation.innerHTML = location
-            pUrl.setAttribute("href", url)
-        }
+    const githubPromise = () => {
+        return new Promise((resolve, reject) => {
+            const xhr = new XMLHttpRequest()
+            xhr.open("GET", "https://api.github.com/users/ullyolima")
+            xhr.send(null)
+            xhr.onreadystatechange = () => {
+                if (xhr.readyState === 4) {
+                    if (xhr.status === 200) {
+                        resolve(JSON.parse(xhr.responseText))
+                    } else {
+                        reject("Error")
+                    }
+                }
+            }
+        })
     }
+
+    githubPromise().then((res) => {
+        const name = res.name
+        const avatar = res.avatar_url
+        const url = res.html_url
+        const location = res.location
+        const login = res.login
+        const bio = res.bio
+
+        htmlImg.setAttribute("src", avatar)
+        h2Name.innerHTML = name
+        pLogin.innerHTML = login
+        pBio.innerHTML = bio
+        pLocation.innerHTML = location
+        pUrl.setAttribute("href", url)
+    }).catch((error) => {
+        console.warn(error)
+    })
 }
